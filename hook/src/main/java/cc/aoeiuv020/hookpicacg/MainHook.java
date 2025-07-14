b3eeb8e72ecc8f5e4c0dbcb0a0fea50097eaac42
MasterOfStar <21274896+MasterOfStar@users.noreply.github.com>
2025-07-14 17:25:06 +0800

    Update MainHook.java
    ai

--- hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
@@ -44,7 +44,7 @@ public class MainHook implements IXposedHookLoadPackage {
         // 拦截广告域名请求（OkHttp3）
         hookOkHttp(lpparam);
 
-        // 原始的各种Hook方法保持不变
+        // 原始Hook方法保持不变
         XposedHelpers.findAndHookMethod(
                 dialogClass,
                 "showAnnouncementAlertDialog",
@@ -60,9 +60,104 @@ public class MainHook implements IXposedHookLoadPackage {
                         param.setResult(null);
                     }
                 });
-        
-        // ... 保持其他原有的Hook方法不变 ...
-        // 注意：这里省略了原有代码中的其他Hook方法，实际代码中需保留
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.utils.views.PopupWebview",
+                lpparam.classLoader,
+                "init",
+                Context.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("beforeHookedMethod: PopupWebview.init(Context)");
+                        param.setResult(null);
+                    }
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.utils.views.BannerWebview",
+                lpparam.classLoader,
+                "init",
+                Context.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("beforeHookedMethod: BannerWebview.init(Context)");
+                        param.setResult(null);
+                    }
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.activities.MainActivity",
+                lpparam.classLoader,
+                "onCreate",
+                Bundle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("afterHookedMethod: MainActivity.onCreate(Bundle)");
+                        View[] buttons_tabbar = (View[]) XposedHelpers.getObjectField(param.thisObject, "buttons_tabbar");
+                        buttons_tabbar[2].setVisibility(View.GONE);
+                        Activity activity = (Activity) param.thisObject;
+                        ViewGroup root = (ViewGroup) ((ViewGroup) (activity.findViewById(android.R.id.content))).getChildAt(0);
+                        for (int i = root.getChildCount() - 1; i >= 0; i--) {
+                            View child = root.getChildAt(i);
+                            if (TextUtils.equals("com.picacomic.fregata.utils.views.BannerWebview", child.getClass().getName())
+                                    || TextUtils.equals("com.picacomic.fregata.utils.views.PopupWebview", child.getClass().getName())) {
+                                root.removeViewAt(i);
+                            }
+                        }
+                    }
+
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.fragments.HomeFragment",
+                lpparam.classLoader,
+                "onCreateView",
+                LayoutInflater.class,
+                ViewGroup.class,
+                Bundle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("afterHookedMethod: HomeFragment.onCreateView");
+                        View viewPager_banner = (View) XposedHelpers.getObjectField(param.thisObject, "viewPager_banner");
+                        ((View) (viewPager_banner.getParent())).setVisibility(View.GONE);
+                        View linearLayout_announcements = (View) XposedHelpers.getObjectField(param.thisObject, "linearLayout_announcements");
+                        linearLayout_announcements.setVisibility(View.GONE);
+                    }
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.adapters.ComicPageRecyclerViewAdapter",
+                lpparam.classLoader,
+                "onCreateViewHolder",
+                ViewGroup.class,
+                int.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("afterHookedMethod: ComicPageRecyclerViewAdapter.onCreateViewHolder");
+                        Object result = param.getResult();
+                        if (!TextUtils.equals(result.getClass().getName(), "com.picacomic.fregata.holders.AdvertisementListViewHolder")) {
+                            return;
+                        }
+                        View webView_ads = (View) XposedHelpers.getObjectField(result, "itemView");
+                        webView_ads.setVisibility(View.GONE);
+                        Object lp = XposedHelpers.newInstance(XposedHelpers.findClass("android.support.v7.widget.RecyclerView$LayoutParams", lpparam.classLoader), 0, 0);
+                        webView_ads.setLayoutParams((ViewGroup.LayoutParams) lp);
+                    }
+                });
+        XposedHelpers.findAndHookMethod("com.picacomic.fregata.adapters.ComicListRecyclerViewAdapter", lpparam.classLoader, "onBindViewHolder", XposedHelpers.findClass("android.support.v7.widget.RecyclerView$ViewHolder", lpparam.classLoader), int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) {
+                Object viewHolder = param.args[0];
+                if (viewHolder.getClass().getSimpleName().equals("AdvertisementListViewHolder")) {
+                    param.setResult(null);
+                    View itemView = (View) XposedHelpers.getObjectField(viewHolder, "itemView");
+                    ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) itemView.getLayoutParams();
+                    // 完全隐藏会影响分页加载的逻辑，所以保留一点，
+                    lp.height = 1;
+                    itemView.setLayoutParams(lp);
+                }
+            }
+        });
     }
 
     /**
@@ -98,7 +193,7 @@ public class MainHook implements IXposedHookLoadPackage {
                             protected void beforeHookedMethod(MethodHookParam param) {
                                 Object request = param.args[1];
                                 String url = (String) XposedHelpers.callMethod(request, "getUrl");
-                                if (isAdUrl(url.toString())) {
+                                if (isAdUrl(url)) {
                                     param.setResult(createTransparentGifResponse());
                                 }
                             }
@@ -114,57 +209,67 @@ public class MainHook implements IXposedHookLoadPackage {
      */
     private void hookOkHttp(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
-            final Class<?> callClass = XposedHelpers.findClass("okhttp3.Call", lpparam.classLoader);
-            final Class<?> requestClass = XposedHelpers.findClass("okhttp3.Request", lpparam.classLoader);
+            final Class<?> callClass = XposedHelpers.findClassIfExists("okhttp3.Call", lpparam.classLoader);
+            if (callClass == null) return;
 
+            final Class<?> requestClass = XposedHelpers.findClass("okhttp3.Request", lpparam.classLoader);
+            final Class<?> interceptorClass = XposedHelpers.findClass("okhttp3.Interceptor", lpparam.classLoader);
+            final Class<?> chainClass = XposedHelpers.findClass("okhttp3.Interceptor$Chain", lpparam.classLoader);
+
+            // Hook Interceptor.Chain.proceed()
             XposedHelpers.findAndHookMethod(
-                    "okhttp3.internal.http.RealInterceptorChain",
-                    lpparam.classLoader,
+                    chainClass,
                     "proceed",
                     requestClass,
                     new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) {
                             Object request = param.args[0];
-                            String url = (String) XposedHelpers.callMethod(
-                                    XposedHelpers.callMethod(request, "url"),
-                                    "toString");
+                            Object url = XposedHelpers.callMethod(request, "url");
+                            String urlString = (String) XposedHelpers.callMethod(url, "toString");
 
-                            if (isAdUrl(url)) {
-                                // 创建伪造的透明图片响应
-                                Class<?> responseClass = XposedHelpers.findClass("okhttp3.Response", lpparam.classLoader);
-                                Class<?> responseBodyClass = XposedHelpers.findClass("okhttp3.ResponseBody", lpparam.classLoader);
-                                Class<?> mediaTypeClass = XposedHelpers.findClass("okhttp3.MediaType", lpparam.classLoader);
-
-                                // 创建MediaType
-                                Object mediaType = XposedHelpers.callStaticMethod(
-                                        mediaTypeClass,
-                                        "parse",
-                                        "image/gif");
-
-                                // 创建ResponseBody
-                                Object responseBody = XposedHelpers.newInstance(
-                                        responseBodyClass,
-                                        mediaType,
-                                        TRANSPARENT_GIF);
-
-                                // 构建伪造响应
-                                Object response = XposedHelpers.newInstance(
-                                        responseClass,
-                                        request,
-                                        200,  // HTTP 200 OK
-                                        "OK",
-                                        null,  // Handshake
-                                        null,  // Headers
-                                        responseBody,
-                                        null,  // CacheControl
-                                        null,  // SentRequestAtMillis
-                                        null,  // ReceivedResponseAtMillis
-                                        null,  // Exchange
-                                        null   // PriorResponse
-                                );
-
-                                param.setResult(response);
+                            if (isAdUrl(urlString)) {
+                                try {
+                                    // 创建伪造的透明图片响应
+                                    Class<?> responseClass = XposedHelpers.findClass("okhttp3.Response", lpparam.classLoader);
+                                    Class<?> responseBodyClass = XposedHelpers.findClass("okhttp3.ResponseBody", lpparam.classLoader);
+                                    Class<?> mediaTypeClass = XposedHelpers.findClass("okhttp3.MediaType", lpparam.classLoader);
+                                    
+                                    // 创建MediaType
+                                    Object mediaType = XposedHelpers.callStaticMethod(
+                                            mediaTypeClass,
+                                            "parse",
+                                            "image/gif");
+                                    
+                                    // 创建ResponseBody
+                                    Object responseBody = XposedHelpers.newInstance(
+                                            responseBodyClass,
+                                            mediaType,
+                                            TRANSPARENT_GIF);
+                                    
+                                    // 构建伪造响应
+                                    Object response = XposedHelpers.callStaticMethod(
+                                            responseClass,
+                                            "newBuilder",
+                                            request,
+                                            200,  // HTTP 200 OK
+                                            "OK",
+                                            null,  // Handshake
+                                            null,  // Headers
+                                            responseBody,
+                                            null,  // CacheControl
+                                            null,  // SentRequestAtMillis
+                                            null,  // ReceivedResponseAtMillis
+                                            null,  // Exchange
+                                            null   // PriorResponse
+                                    );
+                                    
+                                    // 获取最终响应对象
+                                    Object finalResponse = XposedHelpers.callMethod(response, "build");
+                                    param.setResult(finalResponse);
+                                } catch (Throwable t) {
+                                    XposedBridge.log("Error creating fake response: " + t);
+                                }
                             }
                         }
                     });
